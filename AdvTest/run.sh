c64b5c725a1ac2c84d2fa55ceca6e139ccd956c8
szj2935 <541838623@qq.com>
2022-09-26 20:21:59 +0800

    Add files via upload

---
+++ AdvTest/run.sh
@@ -0,0 +1,18 @@
+python  run.py \
+    --output_dir=./saved_models \
+    --model_type=roberta \
+    --config_name=microsoft/graphcodebert-base \
+    --model_name_or_path=microsoft/graphcodebert-base \
+    --tokenizer_name=roberta-base \
+    --do_train \
+    --train_data_file=dataset/train_aug.jsonl \
+    --eval_data_file=dataset/valid.jsonl \
+    --test_data_file=dataset/test.jsonl \
+    --epoch 2 \
+    --block_size 256 \
+    --train_batch_size 16 \
+    --eval_batch_size 64 \
+    --learning_rate 5e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456
